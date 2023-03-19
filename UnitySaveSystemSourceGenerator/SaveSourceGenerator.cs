a43982c5c7e43cef4a2671fffc2e41ddf3d30684
Celezt <59172226+Celezt@users.noreply.github.com>
2023-03-19 23:42:28 +0100

    First commit

---
+++ UnitySaveSystemSourceGenerator/SaveSourceGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Celezt.SaveSystem
+{
+	[Generator]
+	public class SaveSourceGenerator : ISourceGenerator
+	{
+		public void Execute(GeneratorExecutionContext context)
+		{
+			var output = @"
+public class Test 
+{
+	public static void P() => Console.WriteLine(""Hello Code Generation!"");
+}
+";
+			context.AddSource("hello.g.cs", output);
+		}
+
+		public void Initialize(GeneratorInitializationContext context)
+		{
+			context.RegisterForSyntaxNotifications(() => new SaveSyntaxReceiver());
+		}
+	}
+
+	public class SaveSyntaxReceiver : ISyntaxReceiver
+	{
+		public int Index { get; set; }
+
+		public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+		{
+			if (syntaxNode is ClassDeclarationSyntax)
+			{
+
+			}
+		}
+	}
+}
+
